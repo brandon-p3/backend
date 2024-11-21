c2858d44781134ff16a90db25c42f0a51c7a0838
brandon-p3 <brandongustavo350@gmail.com>
2024-11-21 12:34:03 -0600

    Update keys.js

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'controlActividadesUtng'
+        host: 'db-mysql-nyc3-51687-do-user-17602935-0.g.db.ondigitalocean.com',
+        user: 'admin2',
+        password: 'AVNS_KSxUaGxAKmXPsTsvpst',
+        database: 'controlActividadesUtng',
+        port: '25060'
     }
 };
